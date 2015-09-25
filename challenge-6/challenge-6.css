1eaa53f5991d8df0840cc4698c0626095443749c
Mia Chong <monstrousmia@gmail.com>
2015-09-25 01:44:10 -0700

    Create challenge-6.css

---
+++ challenge-6/challenge-6.css
@@ -0,0 +1,23 @@
+// Change theme from light to dark on click
+$("footer #darkTheme").click(function() {;
+      $("header").css({"background":"lightseagreen","border-color":"teal"})
+      $("nav a.selected").css({"color":"mediumaquamarine"});
+      $("nav").css({"background":"teal"})
+      $("body").css({"background-color":"black"});
+      $("#gallery li, #gallery li a p").css({"background-color":"#1d1d1d","color":"mediumpurple"});
+      $("footer, footer a").css({"color":"mediumpurple"});
+      return false;
+    });
+
+
+// Change theme from dark to light on click
+  $("footer #lightTheme").click(function() {;
+    $("header").css({"background":"#918aed","border-color":"#4a3faf"})
+    $("nav a, h1, h2").css({"color":"white"});
+    $("nav a.selected").css({"color":"#918aed"});
+    $("nav").css({"background":"#4a3faf"})
+    $("body").css({"background-color":"white"});
+    $("#gallery li, #gallery li a p").css({"background-color":"#f5f5f5","color":"#adb3b7"});
+    $("footer, footer a").css({"color":"grey"})
+    return false;
+  });
