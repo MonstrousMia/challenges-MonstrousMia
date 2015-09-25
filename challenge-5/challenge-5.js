3e335264dbf40a719d0b2751b82e4764449cbd4e
Mia Chong <monstrousmia@gmail.com>
2015-09-25 01:23:29 -0700

    Create challenge-5.js

---
+++ challenge-5/challenge-5.js
@@ -0,0 +1,11 @@
+var upcase = function upcase(message) {
+    return (message).toUpperCase();
+}
+console.log(upcase("hi there"));
+
+var lowercase = function lowercase(message) {
+    return (message).toLowerCase();
+}
+console.log(lowercase("HI THERE"));
+
+console.log(upcase("it works!"));
